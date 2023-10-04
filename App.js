f2f529d29a9b6774ff3dff14df8aeb64ad5e0b32
Dev <dev@hannahs-mbp.lan>
2023-10-03 22:36:46 -0400

    init test

---
+++ App.js
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './HomeScreen';
+
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+              <Drawer.Screen name="Home" component={HomeScreen} />
+          </Drawer.Navigator>
+      </NavigationContainer>
+  );
+}
+
